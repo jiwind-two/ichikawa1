b23b3c603fbde9227b355422ba9cd538a6b256c8
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-04-09 22:33:40 +0900

    CONST

--- src/index.js
+++ src/index.js
@@ -134,3 +134,17 @@ const newnameArray = nameArr.map((name) => {
   }
 });
 console.log(newnameArray);
+
+/**
+ * 三項演算子
+ */
+const num = 1300;
+
+const formattednum =
+  typeof num === `number` ? num.toLocaleString() : `数値を入力してください`;
+console.log(formattednum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? `100を超えています` : `許容範囲内です`;
+};
+console.log(checkSum(50, 60));
