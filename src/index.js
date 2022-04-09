d38e7c5a1d0fcc781720acd35ca162c998b0d4a5
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-04-09 13:48:29 +0900

    CONST

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ const val5 = ["dog", "cat"];
 val5[0] = "bird";
 val5.push("monky");
 console.log(val5);
+/**
+ * テンプレート文字列
+ */
+
+const name = "市川";
+const age = 69;
+const messege = `私の名前は${name}です、年は${age}です。`;
+console.log(messege);
