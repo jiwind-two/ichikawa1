c2d524a724b06824f943104ae04b32d4fa015f3f
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-04-09 14:56:55 +0900

    CONST

--- src/index.js
+++ src/index.js
@@ -9,8 +9,58 @@ console.log(val5);
 /**
  * テンプレート文字列
  */
+/**
+ * 
 
 const name = "市川";
 const age = 69;
 const messege = `私の名前は${name}です、年は${age}です。`;
 console.log(messege);
+ */
+/**
+ * アロー関数
+ */
+function func2(str) {
+  return str;
+}
+console.log(func2("bbbb"));
+
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("aaaaa"));
+
+const func3 = (str) => {
+  return str;
+};
+console.log(func3("アロー関数"));
+
+const func5 = (str) => str;
+
+console.log(func5("アロー関数3"));
+/*
+ * 分割代入
+ */
+/**
+ * 
+
+const myProfile = {
+  name: "市川",
+  age: 69
+};
+
+const message2 = `私の名前は${myProfile.name}です、歳は${myProfile.age}です。`;
+console.log(message2);
+
+const {name,age}=myProfile;
+const message3=`私の名前は${name}です、歳は${age}です。`;
+console.log(message3);
+ */
+
+const myProfile = ["市川", 69];
+const message30 = `私の名前は${myProfile[0]}です、歳は${myProfile[1]}です。`;
+console.log(message30);
+
+const [name, age] = myProfile;
+const message3 = `私の名前は${name}です、歳は${age}歳です。`;
+console.log(message3);
